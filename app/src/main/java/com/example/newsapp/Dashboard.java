cc392fe4b97646045a43c6387719cf2bdace8d52
Ashish-sah <ashu.sah2000@gmail.com>
2020-09-17 21:01:58 +0530

    Dashboard file updated

--- app/src/main/java/com/example/newsapp/Dashboard.java
+++ app/src/main/java/com/example/newsapp/Dashboard.java
@@ -23,11 +23,13 @@ import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
     Toolbar toolbar;
+    ActionBar actionBar;
     NavigationView navigationView;
     DrawerLayout drawerLayout;
     SwitchCompat drawerSwitch;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +39,15 @@ public class Dashboard extends AppCompatActivity {
         //    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //To change the color of status bar
         getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.toolbar_color));
-
         //set toolbar as action bar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // getSupportActionBar().setDisplayShowHomeEnabled(true);
-        setupNavigationView();
         //handler
         recyclerView = findViewById(R.id.Recycler_View);
         //setting the fixed size of every item in recycler view
         recyclerView.setHasFixedSize(true);
         //setting layout manager for recycler view
-        //layoutManager=
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
       /*  for (int i = 0; i < 10; i++) {
             list_item listItem = new list_item(
@@ -63,12 +62,12 @@ public class Dashboard extends AppCompatActivity {
         //and the adapter class  ie  MyAdapter Class  in fetch data function
         adapter = new MyAdapterClass(this);
         recyclerView.setAdapter(adapter);
+        setupUi();
     }
 
     /*--------To  avoid closing the application on back pressed  -----------------*/
     @Override
     public void onBackPressed() {
-
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
 
@@ -76,92 +75,90 @@ public class Dashboard extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-/*
-    @Override
-    protected void onPostResume() {
-        Intent intent=new Intent(getApplicationContext(),Dashboard.class);
-        startActivity(intent);
-        super.onPostResume();
-    }*/
 
-    public void setupNavigationView() {
+    private void setupUi() {
         //add nav drawer button
-        ActionBar actionBar = getSupportActionBar();
+        actionBar = getSupportActionBar();
         assert actionBar != null;
-        actionBar.setDisplayHomeAsUpEnabled(true);
         //setting the icon
         actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
         drawerLayout = findViewById(R.id.DrawerLayout);
         navigationView = findViewById(R.id.navigation_view);
-
         //tell application to do to and fro in navigation menu when button is clicked to change states and create animations
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
+        setupNavigationView();
+        toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
         /*------------------Navigation Drawer menu-----------------*/
         //Change the view's z  order in the tree, so it's on top of other sibling views.
         // used when their is an error is selecting items
-        navigationView.bringToFront();
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.aboutDev: {
-                        Intent intent = new Intent(getApplicationContext(), About_Developer_activity.class);
-                        startActivity(intent);
-
-                        break;
-                    }
-                    case R.id.dark_mode: {
-                        // This is the menu item that contains your switch layout
-                        item = navigationView.getMenu().findItem(R.id.dark_mode);
-                        drawerSwitch = item.getActionView().findViewById(R.id.Switch);
-                        drawerSwitch.setChecked(true);
-                        drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                            @Override
-                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                                if (isChecked) {
-                                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                                    Toast.makeText(getApplicationContext(), "Switch turned on", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                                    Toast.makeText(getApplicationContext(), "Switch turned off", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                        break;
-                    }
-                    case R.id.share: {
-                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                        //set the type for the sharing thing ie format
-                        sharingIntent.setType("text/plain");
-                        String shareBody = "App project is available at: " + " \n\n Github : https://github.com/Ashish-sah" + "\n\n Follow him on GitHub : https://github.com/Ashish-sah";
-                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
-                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-                        startActivity(Intent.createChooser(sharingIntent, "Share via"));
-                        break;
-                    }
-                    default: { //create toast
-                        Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
-
-                    }
-                }
-                return true;
-            }
-        });
-
+        //  navigationView.bringToFront();
 
     }
 
     //with this method menu icon is clickable and function
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START);
+        //with this method menu icon is clickable
+        navigationView.setCheckedItem(R.id.Switch);
+        navigationView.getMenu().performIdentifierAction(R.id.Switch, 0);
+        if (toggle.onOptionsItemSelected(item)) {
             return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
+    private void setupNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switchScreen(item.getItemId());
+                return true;
+            }
+        });
+    }
+
+    private void switchScreen(int id) {
+        switch (id) {
+            case R.id.aboutDev: {
+                Intent intent = new Intent(getApplicationContext(), About_Developer_activity.class);
+                startActivity(intent);
+                drawerLayout.closeDrawers();
+                finish();
+                break;
+            }
+            case R.id.dark_mode: {
+                // This is the menu item that contains your switch
+                MenuItem item = navigationView.getMenu().findItem(R.id.dark_mode);
+                drawerSwitch = item.getActionView().findViewById(R.id.Switch);
+                //drawerSwitch.setChecked(true);
+                drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                            Toast.makeText(getApplicationContext(), "Switch turned on", Toast.LENGTH_SHORT).show();
+                        } else {
+                            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                            Toast.makeText(getApplicationContext(), "Switch turned off", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                break;
+            }
+            case R.id.share: {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                //set the type for the sharing thing ie format
+                sharingIntent.setType("text/plain");
+                String shareBody = "App project is available at: " + " \n\n Github : https://github.com/Ashish-sah" + "\n\n Follow him on GitHub : https://github.com/Ashish-sah";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                drawerLayout.closeDrawers();
+                break;
+            }
+        }
+    }
 }
