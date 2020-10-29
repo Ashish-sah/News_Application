e4434b83ad401c88f6c52fe3d8e5ebaf6bc2d140
Ashish-sah <ashu.sah2000@gmail.com>
2020-10-29 21:18:33 +0530

    api bug updated

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -40,14 +40,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fetchData() {
-
-        String NewsUrl = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d7fb75a0748247aaaf5682632f840de9";
-
+        String NewsUrl = "https://gnews.io/api/v4/search?q=example&token=6fd62d15cb116f9a7b62cf1b370a7ec8";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, NewsUrl,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
+         /*                   JSONArray   jsonArray = new JSONArray(response);
+                            for (int i=0;i<jsonArray.length();i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                list_item news = new list_item(
+                                        //  newsDetail.getString("urlToImage"),
+                                        jsonObject.getString("state"),
+                                        //newsDetail.getString("description"),
+                                        //newsDetail.getString("content"),
+                                        jsonObject.getString("active"));
+                                list.add(news);
+                            }*/
                             JSONObject jsonObject = new JSONObject(response);
                             //to fetch the array data
                             JSONArray jsonArr = jsonObject.getJSONArray("articles");
@@ -55,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                             for (int i = 0; i < jsonArr.length(); i++) {
                                 JSONObject newsDetail = jsonArr.getJSONObject(i);
                                 list_item news = new list_item(
-                                        newsDetail.getString("urlToImage"),
+                                        newsDetail.getString("image"),
                                         newsDetail.getString("title"),
                                         newsDetail.getString("description"),
                                         newsDetail.getString("content"),
